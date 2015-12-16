4f3e307a33914f889edab956cecb18632e672b96
pr0pellerhead <bojan.gavrovski@gmail.com>
2015-12-16 12:41:10 +0100

    Javascript basics

---
+++ javascript/js1.js
@@ -0,0 +1,82 @@
+
+alert('Паднав на тестот по HTML и CSS!');
+console.log('чекор по чекор.... не се од еднаш!');
+
+typeNum = 1234;
+typeNum2 = 3.14;
+
+typeBool = true;
+typeBool2 = false;
+
+typeObj = {};
+
+typeStr = "bla bla bla bla 123456765756 *($^#%(*&$(*#@)($*";
+
+var1 = "123";
+var2 = 123;
+
+
+
+
+
+// type_obj // snake case
+// typeObj // camel case
+
+// imeIPrezimeNaStudent
+// ime_i_prezime_na_student
+
+// imeiprezimenastudent
+
+ime = 'Bojan';
+console.log(ime);
+
+a = 34;
+b = 12;
+
+c = a * b;
+console.log(c);
+
+// + sobiranje
+// - odzemanje
+// * mnozhenje
+// / delenje
+// % modul
+
+console.log(10 % 3);
+console.log(15 % 4);
+console.log(21 % 2);
+console.log(50 % 4);
+
+
+
+// 2 + 2 = ?
+// 8 - 4 = ?
+// 16 - 12 = ?
+// 2 * 2 = ?
+
+
+// 2 * 10 - 8 = ?
+// 2 * (10 - 8) = ?
+
+// °C  x  9/5 + 32 = °F
+// (°F  -  32)  x  5/9 = °C
+
+
+C = 12;
+console.log(C * 9 / 5 + 32);
+
+F = 53.6;
+console.log((F - 32) * 5 / 9);
+
+// functions
+
+function c2f(c) {
+	return c * 9 / 5 + 32
+}
+
+function f2c(f) {
+	return (f - 32) * 5 / 9;
+}
+
+console.log(c2f(43));
+console.log(f2c(100));
